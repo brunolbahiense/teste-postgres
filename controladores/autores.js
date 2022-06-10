2dd75b835b87b68997d2f3f02e255410c18eaafa
unknown <brunoluisbahiense@gmail.com>
2022-06-10 12:10:56 -0300

    basic functions added

--- controladores/autores.js
+++ controladores/autores.js
@@ -1,17 +1,68 @@
-const conexao = require('../conexao.js')
+const { query } = require('../conexao.js')
 
 const listarAutores = async (req, res) => {
     try{
-        const autores = await conexao.query('SELECT * FROM autores');
-        res.json(autores);
+        const {rows: autores} = await query('SELECT * FROM autores');
+        return res.json(autores);
     } catch(error){
-        res.status(400).json(error)
+        res.status(400).json(error.message)
     }
 }
-const obterAutor = async (req, res) => {}
-const cadastrarAutor = async (req, res) => {}
-const atualizarAutor = async (req, res) => {}
-const excluirAutor = async (req, res) => {}
+const obterAutor = async (req, res) => {
+    const { id } = req.params
+    try{
+        const autor = await query('SELECT * FROM autores WHERE id = $1', [id]);
+        if(autor.rowCount === 0) return res.status(404).json('Autor não encontrado')
+        return res.json(autor.rows[0]);
+
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
+const cadastrarAutor = async (req, res) => {
+    const {nome, idade} = req.body
+    if(!nome) return res.status(400).json('O nome é obrigatório')
+
+    try{
+        const autor = await query('INSERT INTO autores (nome, idade) VALUES ($1, $2)', [nome, idade]) 
+        if(autor.rowCount === 0) return res.status(404).json('Não foi possivel cadastrar o autor')
+        return res.status(201).json('Autor cadastrado com sucesso')
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
+const atualizarAutor = async (req, res) => {
+    const { id } = req.params
+    const {nome, idade} = req.body
+    if(!nome) return res.status(400).json('O nome é obrigatório')
+
+    try{
+        const autor = await query('SELECT * FROM autores WHERE id = $1', [id]);
+        if(autor.rowCount === 0) return res.status(404).json('Autor não encontrado')
+        
+        const autorAtualizado = await query('UPDATE autores set nome = $1, idade = $2 WHERE id = $3', [nome, idade, id]);
+        if(autorAtualizado.rowCount === 0) return res.status(404).json('Não foi possivel atualizar o autor')
+        return res.status(200).json('Autor Atualizado');
+
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
+const excluirAutor = async (req, res) => {
+    const { id } = req.params
+    try{
+        const autor = await query('SELECT * FROM autores WHERE id = $1', [id]);
+        if(autor.rowCount === 0) return res.status(404).json('Autor não encontrado')
+
+        const deletado = await query('DELETE FROM autores WHERE id = $1', [id]);
+        if(deletado.rowCount === 0) return res.status(404).json('Não foi possivel excluir')
+
+        return res.status(200).json('Autor excluido');
+
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
 
 module.exports = {
     listarAutores,
--- controladores/livros.js
+++ controladores/livros.js
@@ -1,8 +1,77 @@
-const listarlivros = async (req, res) => {}
-const obterlivro = async (req, res) => {}
-const cadastrarlivro = async (req, res) => {}
-const atualizarlivro = async (req, res) => {}
-const excluirlivro = async (req, res) => {}
+const { query } = require('../conexao.js')
+
+const listarlivros = async (req, res) => {
+    try{
+        const {rows: livros} = await query('SELECT * FROM livros');
+        return res.json(livros);
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
+const obterlivro = async (req, res) => {
+    const { id } = req.params
+    try{
+        const livro = await query('SELECT * FROM livros WHERE id = $1', [id]);
+        if(livro.rowCount === 0) return res.status(404).json('livro não encontrado')
+        return res.json(livro.rows[0]);
+
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
+const cadastrarlivro = async (req, res) => {
+    const {autor_id, nome, genero, editora, data_publicacao} = req.body
+    if(!nome) return res.status(400).json('O nome é obrigatório')
+
+    try{
+        const sql = `INSERT INTO livros 
+        (autor_id, nome, genero, editora, data_publicacao) 
+        VALUES ($1, $2, $3, $4, $5)`
+        const livroCadastraado = await query(sql, [autor_id, nome, genero, editora, data_publicacao]) 
+        if(livroCadastraado.rowCount === 0) return res.status(404).json('Não foi possivel cadastrar o livro!')
+        return res.status(201).json('Livro cadastrado com sucesso')
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
+const atualizarlivro = async (req, res) => {
+    const {id} = req.params
+    const {autor_id, nome, genero, editora, data_publicacao} = req.body
+    try{
+        const livroCadastrado = await query('SELECT * FROM livros WHERE id = $1', [id]) 
+        if(livroCadastrado.rowCount === 0) return res.status(404).json('Livro não encontrado!')
+
+        const sql = `
+        UPDATE livros SET
+        autor_id = $1,
+        nome = $2,
+        genero = $3, 
+        editora = $4, 
+        data_publicacao = $5
+        WHERE id = $6
+        `
+        const livroAtualizado = await query(sql, [autor_id, nome, genero, editora, data_publicacao, id]);
+        if(livroAtualizado.rowCount === 0) return res.status(404).json('Não foi possivel atualizar o livro!')
+        return res.status(201).json('Livro atualizado com sucesso')
+
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
+const excluirlivro = async (req, res) => {
+    const { id } = req.params
+    try{
+        const livro = await query('SELECT * FROM livros WHERE id = $1', [id]);
+        if(livro.rowCount === 0) return res.status(404).json('livro não encontrado')
+
+        const deletado = await query('DELETE FROM livros WHERE id = $1', [id]);
+        if(deletado.rowCount === 0) return res.status(404).json('Não foi possivel excluir')
+
+        return res.status(200).json('livro excluido');
+    } catch(error){
+        res.status(400).json(error.message)
+    }
+}
 
 module.exports = {
     listarlivros,
